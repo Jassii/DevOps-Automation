4f68c00ec8229ba34957969c5fa5ee89e82d86c5
Jasjeet Singh <root@USGURJASJEESI06.us.deloitte.com>
2024-08-02 14:09:27 +0530

    logger

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -5,12 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 @RestController
 public class DevopsIntegrationApplication {
 
+	Logger logger=LoggerFactory.getLogger(DevopsIntegrationApplication.class);
 	@GetMapping
 	public String message(){
+		logger.info("info message");
+		logger.debug("This is a debug message");
 		return "Hi Welcome to DevOps Automation";
 	}
 
